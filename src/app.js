5fb91fe8c14a715f712b8be6a52233d69b9e0ceb
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2025-05-28 23:26:50 +0530

    API validations

--- src/app.js
+++ src/app.js
@@ -55,10 +55,21 @@ app.delete('/user', async (req,res) => {
   }
 })
 
-app.patch('/user', async (req,res) => {
-  const userId = req.body.userId;
+app.patch('/user/:userId', async (req,res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ['about', 'gender', 'age', 'skillls'];
+    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
+    if(!isUpdateAllowed){
+      throw new Error("Update not allowed");
+    }
+    
+    if(data.skills.length > 10){
+      throw new Error("Skills cannot be more than 10");
+    }
+
     await User.findByIdAndUpdate(userId, data, {
       runValidators: true,
     });
