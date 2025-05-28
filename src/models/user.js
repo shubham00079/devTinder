0e9355b8c40af794c20408701c1a4205cdf2222d
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2025-05-28 22:49:12 +0530

    timestamp

--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,51 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    maxLength: 50
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if(!["male", "female", "others"].includes(value)){
+          throw new Error("gender data is not valid");
+        }
+      } 
+    },
+    about: {
+      type: String,
+      default: 'Good boy',
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    lowercase: true,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if(!["male", "female", "others"].includes(value)){
-        throw new Error("gender data is not valid");
-      }
-    } 
-  },
-  about: {
-    type: String,
-    default: 'Good boy',
-  },
-  skills: {
-    type: [String],
-  },
-});
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 
